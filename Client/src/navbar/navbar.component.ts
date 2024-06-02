2cf9ecd70fda654475540ee5b2aa4a3c6b42584f
motocsky09 <motoc.andreialexandru@gmail.com>
2024-06-02 16:53:34 +0300

    logout commit

--- Client/src/navbar/navbar.component.ts
+++ Client/src/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [
+    UserService
+  ]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+constructor(private service: UserService, 
+            private router: Router) { }
+
+ngOnInit(): void {
+}
+
+isLog(): boolean
+{
+  return this.service.isLogged();
+}
+
+onLogout()
+{
+  this.service.logout();
+  return this.router.navigate(['/user/login']);
+}
 }
